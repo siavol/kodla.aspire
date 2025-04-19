527988aaafdb25f2e1d3310ad545e06bf70f7be9
Ilya <siavol@gmail.com>
2025-04-19 12:46:43 +0300

    use db creation script

--- AppHost/Program.cs
+++ AppHost/Program.cs
@@ -15,7 +15,33 @@ var sqlServer = builder.AddSqlServer("sqlserver")
 builder.AddProject<Kodla_Api>("api-service")
     .WithReference(rabbitmq).WaitFor(rabbitmq);
 
-var meetupDb = sqlServer.AddDatabase("meetup-db");
+const string meetupDbName = "meetup-db";
+const string meetupDbCreateScript = $$"""
+    IF DB_ID('{{meetupDbName}}') IS NULL
+        CREATE DATABASE [{{meetupDbName}}];
+    GO
+
+    -- Use the database
+    USE [{{meetupDbName}}];
+    GO
+
+    -- Create tables
+    CREATE TABLE [Meetup] (
+        [Id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+        [Name] NVARCHAR(255) NOT NULL,
+        [Description] NVARCHAR(MAX) NOT NULL,
+        [Date] DATETIME2 NOT NULL,
+        [MaxAttendees] INT NOT NULL
+    );
+    GO
+
+    -- Seeding data
+    INSERT INTO [Meetup] ([Name], [Description], [Date], [MaxAttendees])
+    VALUES
+        ('Best Meetup', 'The best meetup in Uusimmaa area. Learn some cool stuffю', '2025-06-10 10:00:00', 20);
+""";
+var meetupDb = sqlServer.AddDatabase(meetupDbName)
+    .WithCreationScript(meetupDbCreateScript);
 builder.AddProject<Kodla_Meetup_Processor>("meetup-processor-service")
     .WithReference(rabbitmq).WaitFor(rabbitmq)
     .WithReference(meetupDb).WaitFor(meetupDb);
--- Kodla.Meetup.Processor/Program.cs
+++ Kodla.Meetup.Processor/Program.cs
@@ -19,17 +19,4 @@ builder.AddSqlServerDbContext<MeetupDbContext>(connectionName: "meetup-db");
 
 var host = builder.Build();
 
-MigrateDatabase(host.Services);
-
 host.Run();
-
-
-static void MigrateDatabase(IServiceProvider serviceProvider)
-{
-    using var scope = serviceProvider.CreateScope();
-    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
-    var context = scope.ServiceProvider.GetRequiredService<MeetupDbContext>();
-    logger.LogInformation("Migrating database...");
-    context.Database.Migrate();
-    logger.LogInformation("Database migration completed.");
-} 
