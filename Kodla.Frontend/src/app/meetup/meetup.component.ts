89f2b1e0e6cd9cd77fa7bf0309bf0e3f15f9eba3
Ilya <siavol@gmail.com>
2025-04-21 20:11:44 +0300

    show meetup page

--- Kodla.Frontend/src/app/meetup/meetup.component.ts
+++ Kodla.Frontend/src/app/meetup/meetup.component.ts
@@ -1,20 +1,33 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Meetup } from '../../model/meetup-types';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-meetup',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './meetup.component.html',
   styleUrl: './meetup.component.css'
 })
-export class MeetupComponent {
-  constructor(private route: ActivatedRoute) {}
+export class MeetupComponent implements OnInit {
+  constructor(
+    private route: ActivatedRoute,
+    private http: HttpClient) {}
 
-  meetupId: string | null = null;
+  // meetupId: string | null = null;
+  meetup?: Meetup;
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
-      this.meetupId = params.get('meetupId');
+      const meetupId = params.get('meetupId');
+
+      this.http.get<Meetup>(`api/meetups/${meetupId}`).subscribe({
+        next: result => this.meetup = result,
+        error: err => console.error('Error fetching meetup details:', err)
+      });
     });
   }
 }
